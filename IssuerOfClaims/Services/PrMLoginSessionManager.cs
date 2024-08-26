4dfbdb375b0e65b12f28e4367053d875eb6a5938
doremon1381 <doremon1381@gmail.com>
2024-08-26 13:19:28 +0700

    - IS: remove files - IS: update description

--- IssuerOfClaims/Services/PrMLoginSessionManager.cs
+++ IssuerOfClaims/Services/PrMLoginSessionManager.cs
@@ -114,10 +114,11 @@ namespace IssuerOfClaims.Services
         }
 
         // TODO:
-        // Create new session's object whenever a request involve with identity services is call, save it into a static list
-        // - set for it authorization code when authorization code flow is initiated, add code challenger, add id token, access token expired time and access token follow this flow
+        // Create new session's object whenever a request involve with identity services is called
+        // - set for it authorization code when authorization code flow is initiated, add code challenger, add id token, access token expired time and access token when a request for access token include grant_type is called
         // - set for it id token and access token when implicit grant (with form_post or not) is initiated
         // =>  after everything is done following a particular flow which is used for authentication, save this session object to database
+        // - TODO: these following few lines is good ideal, I think, but I have problems when trying to implement it, so for now, I save everything in db
         // * Note: I don't want to save it when initiate authentication process and get it from database when it's call,
         //       : because, a particular session is used along with authentication process will be the latest, and search for it in db can create performance cost when this server is used long enough.
         //       : instead of search from db, save 100 session in used, and get it from memory (from authorization code, or id_token) is easier than query 100 object from 100.000 object table...
